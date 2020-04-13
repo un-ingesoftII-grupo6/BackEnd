3242a703c6586b29dab5a560bfd8942329bb6a70
Miguel Alejandro Peña <miapenahu@unal.edu.co>
2020-04-13 08:48:08 -0500

    Develop branch created.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');
 
 // routes
 app.use(require('./routes/index'));
+
 // static files
 
 // listening the server 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index'/*, { title : 'UN Wallet'}*/);
+    
     //console.log(path.join(__dirname, 'views/index.html'));
 });
 
