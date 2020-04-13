e41c4f8fd2fb2175b53af0412ed305e9feec6778
Miguel A. Peña <miapenahu@unal.edu.co>
2020-04-13 17:22:02 -0500

    Merging Front End develop to Back End.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // setings 
-app.set('port', 8080)
+app.set('port', process.env.PORT || 8080)
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs');
 
-// middlewares 
+// view petitions
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 // routes
 app.use(require('./routes/index'));
 
 // static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // listening the server 
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
+
+var users = [];
 
 router.get('/', (req, res) => {
-    res.render('index'/*, { title : 'UN Wallet'}*/);
-    
-    //console.log(path.join(__dirname, 'views/index.html'));
+    res.render('index.html', { title : 'UN Wallet' });
 });
 
+router.get('/login', (req, res) => {
+    res.render('login.html', { title : 'Log in' });
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup.html', { title : 'Sign up' });
+});
+
+router.get('/wallet', (req, res) => {
+    res.render('/wallet.html');
+});
+
+router.post('/login', (req, res) => {
+    // Acá va la validación con la base de datos
+    var flag = false;
+    var user = null;
+
+    for(var i = 0; i < users.length; i++) { //búsqueda de la existencia de un usuario (por el momento se trabaja en memoria)
+        if(req.body.email == users[i].email && req.body.password == users[i].password) {
+            user = users[i];
+            flag = true;
+            break;
+        }
+    }
+
+    if(flag) {
+        res.render('wallet.html', user);
+    } else {
+        res.send(400).send('El ususario no existe');
+    }  
+});
+
+router.post('/signup', (req, res) => {
+    // Acá va la validación con la base de datos 
+    if(req.body.password != req.body.confirmPassword  || req.body.email != req.body.confirmEmail) {
+        res.send(400).send("Las contraseñas y los emails deben ser iguales");
+    } else {
+        let wallet = { // Se debe agregar la wallet a la base de datos
+            id_wallet : uuid.v4(),
+            balance : 0.0,
+            state : true
+        };
+
+        let user = { // El usuario debe ser guardado en la base de datos si todo está correcto
+            name : req.body.name,
+            lastName : req.body.lastName,
+            email : req.body.email,
+            password : req.body.password,
+            wallet_user : wallet
+        };
+
+        users.push(user);
+        res.render('wallet.html', user);
+    }
+});
+
+// Export module
 module.exports = router;
